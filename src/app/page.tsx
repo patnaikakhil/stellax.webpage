a6d7e419c66fecb64c9bafaf264e85bc53632b1a
Akhil Patnaik Raghupatruni <136100744+patnaikakhil@users.noreply.github.com>
2024-10-04 20:04:44 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,11 @@ export default function Home() {
           Hello!!
         </div>
         <div>
+          <i>
+            <b>
           We are coming soon!
+              </b>
+            </i>
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
